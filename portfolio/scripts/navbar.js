65a8cccf3fcbe5c6bdb51fee2c577e23b31524a5
jonathanwong110 <jonathan.wong110@gmail.com>
2021-02-28 13:21:48 -0500

    nav link will now highlight based on where you are on the page, minor changes

--- portfolio/scripts/navbar.js
+++ portfolio/scripts/navbar.js
@@ -1,15 +1,28 @@
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("navbar").style.top = "0";
-  } else {
-    document.getElementById("navbar").style.top = "-65px";
-  }
-  prevScrollpos = currentScrollPos;
-}
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-65px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
 
-$('.nav-link').on('click',function() {
+$(window).scroll(function () {
+  var position = window.pageYOffset;
+  $('.section').each(function () {
+    var target = $(this).offset().top;
+    var id = $(this).attr('id');
+    var navLinks = $('.navbar-nav.ml-auto li a');
+    if (position >= target) {
+      navLinks.removeClass('active');
+      $('.navbar-nav.ml-auto li a[href*=\\#' + id + ']').addClass('active');
+    }
+  });
+});
+
+$('.nav-link').on('click', function () {
   $('.navbar-collapse').collapse('hide');
 });
 
