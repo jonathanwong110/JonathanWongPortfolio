bf4f8c5e95161c2165621e488af56dfd595a2212
jonathanwong110 <jonathan.wong110@gmail.com>
2021-03-06 12:57:07 -0500

    minor changes

--- portfolio/scripts/scroll.js
+++ portfolio/scripts/scroll.js
@@ -1,4 +1,4 @@
 function scrollToSection(x) {
-  var elementPosition = document.getElementById(x).offsetTop;
+  let elementPosition = document.getElementById(x).offsetTop;
   window.scrollTo(0, elementPosition);
 }
