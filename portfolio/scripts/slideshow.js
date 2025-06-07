da3c72f545bc808cd330a94f0577024a80c14cf5
jonathan.wong110 <jonathan.wong110@gmail.com>
2025-06-07 17:40:38 -0400

    updated about/experience, added project, removed slideshows for now

--- portfolio/scripts/slideshow.js
+++ portfolio/scripts/slideshow.js
@@ -1,5 +1,10 @@
-var slideIndex = [1,1];
-var slideId = ["portfolio-project-images-international-commodities", "portfolio-project-images-gidi", "portfolio-project-images-bookstore"]
+var slideIndex = [1, 1];
+var slideClass = [
+  "portfolio-project-images-coop-capstone",
+  "portfolio-project-images-international-commodities",
+  "portfolio-project-images-gidi",
+  "portfolio-project-images-bookstore"
+]
 showSlides(1, 0);
 showSlides(1, 1);
 
@@ -9,11 +14,11 @@ function plusSlides(n, no) {
 
 function showSlides(n, no) {
   var i;
-  var x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
-  if (n < 1) {slideIndex[no] = x.length}
+  var x = document.getElementsByClassName(slideClass[no]);
+  if (n > x.length) { slideIndex[no] = 1 }
+  if (n < 1) { slideIndex[no] = x.length }
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+    x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no] - 1].style.display = "block";
 }
